e919afdb68ce650a8cf84cd1db801dfaedb23aa6
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-06-04 21:28:24 -0500

    config added

--- src/main/java/ec/dev/samagua/ekumen_cloud_gateway/EkumenCloudGatewayApplication.java
+++ src/main/java/ec/dev/samagua/ekumen_cloud_gateway/EkumenCloudGatewayApplication.java
@@ -2,8 +2,10 @@ package ec.dev.samagua.ekumen_cloud_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EkumenCloudGatewayApplication {
 
     public static void main(String[] args) {
