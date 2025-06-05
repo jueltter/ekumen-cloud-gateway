827039a28bcca9c2aa2e27d67bd5d712a17dfbc4
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-06-04 21:24:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ekumen-cloud-gateway
+
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/main/java/ec/dev/samagua/ekumen_cloud_gateway/EkumenCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package ec.dev.samagua.ekumen_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkumenCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkumenCloudGatewayApplication.class, args);
+    }
+
+}
